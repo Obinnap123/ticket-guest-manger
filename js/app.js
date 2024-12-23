a5fc20a747d0ab23fe08958cc63e70c047be51e2
Paul Obinna <paulobinna493@gmail.com>
2024-12-23 22:54:32 +0100

    added sixth-section

--- js/app.js
+++ js/app.js
@@ -131,8 +131,7 @@ T&AM is such a blessing!
   },
   {
     category: "Easy Check In By My Event Attendees",
-    testimonial:
-    `T&AM must have had the masses by heart while developing their commission structure, as it's so economical and affordable, which has always given me the chance to take care of my event expenses easily with enough ticket sales funds.
+    testimonial: `T&AM must have had the masses by heart while developing their commission structure, as it's so economical and affordable, which has always given me the chance to take care of my event expenses easily with enough ticket sales funds.
     T&AM is such a blessing!
     `,
     rating: 5,
@@ -251,4 +250,291 @@ function initializeSwiper() {
 document.addEventListener("DOMContentLoaded", () => {
   renderSwiperSlides();
   initializeSwiper();
+});
+
+function createCard(data) {
+  // Create the card element
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  // Image
+  const image = document.createElement("img");
+  image.src = data.imageSrc || "https://via.placeholder.com/300";
+  image.alt = data.imageAlt || "Card Image";
+  card.appendChild(image);
+
+  // Card Content
+  const cardContent = document.createElement("div");
+  cardContent.classList.add("card-content");
+
+  // Button
+  const button = document.createElement("button");
+  button.textContent = data.buttonText || "Click Me";
+  button.style.marginBottom = "10px";
+  button.style.backgroundColor = "rgba(249, 164, 83, 1)";
+  button.style.cursor = "pointer";
+
+  // Create the <a> tag
+  const link = document.createElement("a");
+  link.href = data.buttonLink || "#"; // Replace with the actual URL or "#" if none provided
+  link.target = "_blank"; // Opens the link in a new tab
+  link.style.textDecoration = "none"; // Ensures no underline
+
+  // Append the button to the <a> tag
+  link.appendChild(button);
+
+  // Append the <a> tag wherever needed (e.g., cardContent)
+  cardContent.appendChild(link);
+
+  // button.style.backgroundColor = "rgba(249, 164, 83, 1)";
+  // Subtitle
+  const subtitle = document.createElement("h3");
+  subtitle.textContent = data.subtitle || "Default Subtitle";
+  cardContent.appendChild(subtitle);
+
+  // Horizontal Rule
+  cardContent.appendChild(createHorizontalRule());
+
+  // Price Row
+  const priceRow = createRow("Price", `₦${data.price}`);
+  cardContent.appendChild(priceRow);
+  cardContent.appendChild(createHorizontalRule());
+
+  // Slots Left Row
+  const slotsRow = createRow("Slots Left", data.slotsLeft);
+  cardContent.appendChild(slotsRow);
+  cardContent.appendChild(createHorizontalRule());
+
+  // Date Row
+  const dateRow = createRow("Date", `${data.date} (${data.time})`);
+  cardContent.appendChild(dateRow);
+  cardContent.appendChild(createHorizontalRule());
+
+  // Social Media Icons
+  const socialIcons = document.createElement("div");
+  socialIcons.classList.add("social-icons");
+
+  data.socialMedia.forEach((icon) => {
+    const link = document.createElement("a");
+    link.href = icon.link || "#"; // Replace with the actual link, or use "#" if none is provided
+    link.target = "_blank"; // Opens the link in a new tab
+    link.style.marginRight = "10px";
+
+    const socialIcon = document.createElement("img");
+    socialIcon.src = icon.src;
+    socialIcon.alt = icon.alt || "Social Icon";
+
+    // Append the image to the link
+    link.appendChild(socialIcon);
+
+    // Append the link to the socialIcons container
+    socialIcons.appendChild(link);
+  });
+
+  cardContent.appendChild(socialIcons);
+  card.appendChild(cardContent);
+
+  return card;
+}
+
+function createRow(leftText, rightText) {
+  const row = document.createElement("div");
+  row.style.display = "flex";
+  row.style.justifyContent = "space-between";
+
+  const left = document.createElement("p");
+  left.textContent = leftText;
+
+  const right = document.createElement("p");
+  right.textContent = rightText;
+
+  row.appendChild(left);
+  row.appendChild(right);
+  return row;
+}
+
+function createHorizontalRule() {
+  const hr = document.createElement("hr");
+  hr.classList.add("horizontal-rule");
+  return hr;
+}
+
+// Example Data
+const cardDataArray = [
+  {
+    imageSrc: "../images/glass-img.png",
+    imageAlt: "Image 1",
+    buttonText: "Party",
+    buttonLink: "https://ticketguestmanager.com/events-category/5/Party",
+    subtitle: "Abeokuta palm wine hangout",
+    price: "1,500.00",
+    slotsLeft: "120",
+    date: "24 Nov. 2024",
+    time: "10:30am",
+    socialMedia: [
+      {
+        src: "../images/facebook-icon-orange.svg",
+        alt: "facebook-icon",
+        link: "https://web.facebook.com/eventticketsmanager/",
+      },
+      {
+        src: "../images/instagram-icon-orange.svg",
+        alt: "instagram-icon",
+        link: "https://www.instagram.com/ticket_and_attendee_manager/",
+      },
+      {
+        src: "../images/twitter-icon-orange.svg",
+        alt: "twitter-icon",
+        link: "https://x.com/Guestsmanager",
+      },
+      // { src: "https://via.placeholder.com/24", alt: "Twitter" },
+    ],
+  },
+  {
+    imageSrc: "../images/book-img.png",
+    imageAlt: "Image 2",
+    buttonText: "Education",
+    buttonLink: "https://ticketguestmanager.com/events-category/22/Education",
+    subtitle: "Learning lounge: sip & study",
+    price: "2,000.00",
+    slotsLeft: "90",
+    date: "25 Nov. 2024",
+    time: "11:00am",
+    socialMedia: [
+      {
+        src: "../images/facebook-icon-orange.svg",
+        alt: "facebook-icon",
+        link: "https://web.facebook.com/eventticketsmanager/",
+      },
+      {
+        src: "../images/instagram-icon-orange.svg",
+        alt: "instagram-icon",
+        link: "https://www.instagram.com/ticket_and_attendee_manager/",
+      },
+      {
+        src: "../images/twitter-icon-orange.svg",
+        alt: "twitter-icon",
+        link: "https://x.com/Guestsmanager",
+      },
+    ],
+  },
+  {
+    imageSrc: "../images/birthday-img.png",
+    imageAlt: "Image 3",
+    buttonText: "Birthday",
+    buttonLink: "https://ticketguestmanager.com/events-category/18/Birthday",
+    subtitle: "Birthday bash at palm oasis",
+    price: "3,000.00",
+    slotsLeft: "70",
+    date: "26 Nov. 2024",
+    time: "12:00pm",
+    socialMedia: [
+      {
+        src: "../images/facebook-icon-orange.svg",
+        alt: "facebook-icon",
+        link: "https://web.facebook.com/eventticketsmanager/",
+      },
+      {
+        src: "../images/instagram-icon-orange.svg",
+        alt: "instagram-icon",
+        link: "https://www.instagram.com/ticket_and_attendee_manager/",
+      },
+      {
+        src: "../images/twitter-icon-orange.svg",
+        alt: "twitter-icon",
+        link: "https://x.com/Guestsmanager",
+      },
+    ],
+  },
+  {
+    imageSrc: "../images/lady-writing-img.png",
+    imageAlt: "Image 3",
+    buttonText: "Conference",
+    buttonLink: "https://ticketguestmanager.com/events-category/14/conference",
+    subtitle: "Innovative minds conference",
+    price: "3,000.00",
+    slotsLeft: "70",
+    date: "26 Nov. 2024",
+    time: "12:00pm",
+    socialMedia: [
+      {
+        src: "../images/facebook-icon-orange.svg",
+        alt: "facebook-icon",
+        link: "https://web.facebook.com/eventticketsmanager/",
+      },
+      {
+        src: "../images/instagram-icon-orange.svg",
+        alt: "instagram-icon",
+        link: "https://www.instagram.com/ticket_and_attendee_manager/",
+      },
+      {
+        src: "../images/twitter-icon-orange.svg",
+        alt: "twitter-icon",
+        link: "https://x.com/Guestsmanager",
+      },
+    ],
+  },
+  {
+    imageSrc: "../images/light-img.png",
+    imageAlt: "Image 3",
+    buttonText: "Awards",
+    buttonLink: "https://ticketguestmanager.com/events-category/19/Awards",
+    subtitle: "Euphoria under the stars",
+    price: "3,000.00",
+    slotsLeft: "70",
+    date: "26 Nov. 2024",
+    time: "12:00pm",
+    socialMedia: [
+      {
+        src: "../images/facebook-icon-orange.svg",
+        alt: "facebook-icon",
+        link: "https://web.facebook.com/eventticketsmanager/",
+      },
+      {
+        src: "../images/instagram-icon-orange.svg",
+        alt: "instagram-icon",
+        link: "https://www.instagram.com/ticket_and_attendee_manager/",
+      },
+      {
+        src: "../images/twitter-icon-orange.svg",
+        alt: "twitter-icon",
+        link: "https://x.com/Guestsmanager",
+      },
+    ],
+  },
+  {
+    imageSrc: "../images/hand-img.png",
+    imageAlt: "Image 3",
+    buttonText: "Charity",
+    buttonLink: "https://ticketguestmanager.com/events-category/20/Charity",
+    subtitle: "Sip for a cause: charity palm wine fest",
+    price: "3,000.00",
+    slotsLeft: "70",
+    date: "26 Nov. 2024",
+    time: "12:00pm",
+    socialMedia: [
+      {
+        src: "../images/facebook-icon-orange.svg",
+        alt: "facebook-icon",
+        link: "https://web.facebook.com/eventticketsmanager/",
+      },
+      {
+        src: "../images/instagram-icon-orange.svg",
+        alt: "instagram-icon",
+        link: "https://www.instagram.com/ticket_and_attendee_manager/",
+      },
+      {
+        src: "../images/twitter-icon-orange.svg",
+        alt: "twitter-icon",
+        link: "https://x.com/Guestsmanager",
+      },
+    ],
+  },
+  // Add data for 3 more cards
+];
+
+// Render all cards
+const cardContainer = document.getElementById("cardContainer");
+cardDataArray.forEach((data) => {
+  cardContainer.appendChild(createCard(data));
 });
